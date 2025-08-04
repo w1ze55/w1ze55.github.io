da7fa41da86be229bf4bd1001dcdaf7b650ead03
w1ze55 <cacauezin@gmail.com>
2025-08-04 20:43:59 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -18,11 +18,12 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <h2>My firts page with github actions - Caue</h2>
+      <h3>Um texto legal</h3>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
+        <p> 
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
